16e80cab9d426f5663b885dd625bad4b5ff83997
lesw1216 <lesw1216@gmail.com>
2024-11-13 16:14:53 +0900

    init: initial project

---
+++ src/main/java/lesw/fit_log/FitLogApplication.java
@@ -0,0 +1,13 @@
+package lesw.fit_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lesw/fit_log/FitLogApplicationTests.java
@@ -0,0 +1,13 @@
+package lesw.fit_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
