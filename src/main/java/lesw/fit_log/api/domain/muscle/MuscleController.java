c4959eefe59419aad69ec3dc09ad6d736804bf2f
sangwoo <lesw1216@gmail.com>
2024-11-18 22:52:12 +0900

    refactor: Muscle 변경
    1. MuscleMapper가 MuscleRepository를 상속받지 않게 변경
    2. MuscleController.class의 getMuscle()의 return 값을 Rest API 구조로 변경

--- src/main/java/lesw/fit_log/api/domain/muscle/MuscleController.java
+++ src/main/java/lesw/fit_log/api/domain/muscle/MuscleController.java
@@ -1,7 +1,10 @@
 package lesw.fit_log.api.domain.muscle;
 
 import lesw.fit_log.api.domain.muscle.repository.MuscleMapper;
+import lesw.fit_log.api.response.RestResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +19,11 @@ public class MuscleController {
     private final MuscleMapper muscleMapper;
 
     @GetMapping("/muscle")
-    public List<MuscleDTO> getMuscle() {
-        return muscleMapper.findAll();
+    public ResponseEntity<RestResponse<List<MuscleDTO>>> getMuscle() {
+        List<MuscleDTO> findMuscles = muscleMapper.findAll();
+
+        RestResponse<List<MuscleDTO>> restResponse = RestResponse.of(HttpStatus.OK, "전체 조회 성공", findMuscles);
+        return new ResponseEntity<>(restResponse, restResponse.getStatus());
     }
 
 }
